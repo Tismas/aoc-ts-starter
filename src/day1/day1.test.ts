291adcc62b5632678e7e3e479d9f8346bc873896
Adam Golan <adam.golan@allegro.pl>
2021-08-13 13:51:30 +0200

    tests

---
+++ src/day1/day1.test.ts
@@ -0,0 +1,10 @@
+import { part1, part2 } from "./solution";
+
+describe("day1", () => {
+  it("part1", () => {
+    expect(part1("Hello World")).toEqual("Hello AoC");
+  });
+  it("part2", () => {
+    expect(part2("Hello World")).toEqual("Part 2 AoC");
+  });
+});
---
+++ src/day2/day2.test.ts
@@ -0,0 +1,10 @@
+import { part1, part2 } from "./solution";
+
+describe("day2", () => {
+  it("part1", () => {
+    expect(part1("Hello World")).toEqual("Hello Day 2");
+  });
+  it("part2", () => {
+    expect(part2("Hello World")).toEqual("Day 2 Part 2 AoC");
+  });
+});
